474f4c6c9775df0c18caf8f8cabb041cb318aa30
AltanEmuce <altanemuce61@gmail.com>
2023-02-19 21:45:58 +0300

    ioc added

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,17 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<ITodoService, TodoManager>();
+builder.Services.AddSingleton<ITodoDal, TodoDal>();
+builder.Services.AddSingleton<IUserService, UserManager>();
+builder.Services.AddSingleton<IUserDal, UserDal>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
